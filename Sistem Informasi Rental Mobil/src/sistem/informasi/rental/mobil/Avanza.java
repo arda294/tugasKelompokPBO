69de1b3312561531760d5285bf1193f11224dccf
arda294 <114855785+arda294@users.noreply.github.com>
2022-10-04 18:46:25 +0800

    Update Avanza.java

--- Sistem Informasi Rental Mobil/src/sistem/informasi/rental/mobil/Avanza.java
+++ Sistem Informasi Rental Mobil/src/sistem/informasi/rental/mobil/Avanza.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package sistem.informasi.rental.mobil;
 
-/**
- *
- * @author ardap
- */
 public class Avanza extends Mobil {
     Avanza() {
         setHargaSewa(400000);
