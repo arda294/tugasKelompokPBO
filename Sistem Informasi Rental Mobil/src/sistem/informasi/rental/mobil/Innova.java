a4c8bf9f70c3b193f696210713f58f68cb35c585
arda294 <114855785+arda294@users.noreply.github.com>
2022-10-04 18:47:03 +0800

    Update Innova.java

--- Sistem Informasi Rental Mobil/src/sistem/informasi/rental/mobil/Innova.java
+++ Sistem Informasi Rental Mobil/src/sistem/informasi/rental/mobil/Innova.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package sistem.informasi.rental.mobil;
 
-/**
- *
- * @author ardap
- */
 public class Innova extends Mobil{
     Innova() {
         setHargaSewa(400000);
